7c8ac7d550e17012d7ca4793e728127793c593d3
lolcode <1123603312@qq.com>
2023-08-22 14:06:13 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import CModal from './components/CModal.vue'
+
+const open = ref(false)
+
+</script>
+
+<template>
+  <div>
+    <button @click="open = true">打开业务弹窗</button>
+    <CModal v-model:open="open"></CModal>
+  </div>
+</template>
---
+++ src/components/CModal.vue
@@ -0,0 +1,27 @@
+<script lang="ts" setup>
+import { useAttrs } from 'vue'
+import { Modal, type ModalProps } from 'ant-design-vue'
+
+
+// ModalProps不具有onUpdate:open属性
+// 以下是封装Modal的使用场景
+
+const attrs = useAttrs() as ModalProps
+const confirm = () => {
+  // ...
+  // ...
+
+  // 关闭弹窗
+  attrs['onUpdate:open']?.(false)  // ts报错ModalProps中缺少onUpdate:open 属性
+  // 有onUpdate:visible参数但，ant-design-vue下个版本会删除visible(v-model)参数不建议使用
+}
+</script>
+
+<template>
+  <Modal>
+    <div>业务展示...</div>
+    <template #footer>
+      <button @click="confirm">完成</button>
+    </template>
+  </Modal>
+</template>
